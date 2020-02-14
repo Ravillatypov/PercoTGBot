c0ea1c02aab02ec23873728ee1722c4d77296fb9
Ravil Latypov <ravillatypov12@gmail.com>
2020-02-14 04:55:06 +0300

    add callback for open/close doors

--- app/telegram/start.py
+++ app/telegram/start.py
@@ -1,8 +1,10 @@
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 from app.models import User, Door
 from app.perco import PercoClient
-from app.settings import dp, ADMIN_USERNAME
+from app.settings import dp, ADMIN_USERNAME, bot
+
+perco = PercoClient()
 
 
 @dp.message_handler(commands=['start'])
@@ -29,7 +31,6 @@ async def start(message: Message):
 async def update_doors(message: Message):
     if message.chat.username != ADMIN_USERNAME:
         return await message.reply('У вас не достаточно прав для данной операции')
-    perco = PercoClient()
     doors = await perco.get_doors()
     for data in doors:
         await Door.get_or_create(id=data.get('id', 0), defaults={'name': data.get('name', '-')})
@@ -39,4 +40,23 @@ async def update_doors(message: Message):
 async def get_doors(message: Message):
     if message.chat.username == ADMIN_USERNAME:
         doors = await Door.all()
-        await message.reply(f'количество дверей {len(doors)}: ' + ', '.join((i.name for i in doors)))
+        markup = InlineKeyboardMarkup(row_width=1)
+        for door in doors:
+            markup.add(InlineKeyboardButton(f'Открыть "{door.name}"', callback_data=f'door_open_{door.id}'))
+            markup.add(InlineKeyboardButton(f'Закрыть "{door.name}"', callback_data=f'door_close_{door.id}'))
+        await bot.send_message(message.chat.id, 'Доступные двери:', reply_markup=markup)
+
+
+@dp.callback_query_handler(lambda x: 'door_open_' in x.data)
+async def callback_door_open(callback_query: CallbackQuery):
+    door_id = int(callback_query.data.replace('door_open_', ''))
+    await perco.open_door(door_id)
+    await bot.answer_callback_query(callback_query.id, 'Дверь открыта')
+
+
+@dp.callback_query_handler(lambda x: 'door_close_' in x.data)
+async def callback_door_close(callback_query: CallbackQuery):
+    door_id = int(callback_query.data.replace('door_close_', ''))
+    await perco.close_door(door_id)
+    await bot.answer_callback_query(callback_query.id, 'Дверь закрыта')
+
