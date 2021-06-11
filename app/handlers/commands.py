974ab1d66b02864091d93d4752b76e0d2448feb9
Ravil Latypov <ravillatypov12@gmail.com>
2021-06-11 12:24:05 +0300

    add reply message

--- app/handlers/commands.py
+++ app/handlers/commands.py
@@ -5,6 +5,7 @@ from app.decorators import admin
 from app.helpers import check_admin_user, send_user_edit_message, send_available_doors, get_users_markup
 from app.models import User, Door
 from app.settings import dp, ADMIN_USERNAME, logger
+from app.perco import PercoClient
 
 
 @dp.message_handler(commands=['start'])
@@ -34,13 +35,14 @@ async def start(message: Message):
 @dp.message_handler(commands=['updateDoors', 'sync_doors'])
 @admin
 async def update_doors(message: Message, **kwargs):
-    perco = dp['perco']
-    doors = await get_doors()
-    for data in doors:
+    perco: PercoClient = dp['perco']
+
+    for data in await perco.get_doors():
         door, _ = await Door.get_or_create(id=data.get('id', 0), defaults={'name': data.get('name', '-')})
         if door.name != data.get('name', '-'):
             door.name = data.get('name', '-')
             await door.save(update_fields=['name'])
+
     await message.reply('Обновил')
 
 
@@ -64,6 +66,9 @@ async def get_users(message: Message, **kwargs):
 async def force_update_doors(message: Message, **kwargs):
     users = await User.all()
 
+    errors = []
+    success = 0
+
     for user in users:
         try:
             await send_available_doors(user.chat_id, dp['perco'])
@@ -71,3 +76,16 @@ async def force_update_doors(message: Message, **kwargs):
             await user.delete()
         except Exception as e:
             logger.warning(f'cant update doors for user={user}, error="{e}"')
+            errors.append(f'"{e}"')
+
+    fail = len(errors)
+    errors = ', '.join(set(errors))
+    text = 'Список дверей не отправлено'
+
+    if success:
+        text = f'Успешно отправлено {success} пользователям.'
+
+    if fail:
+        text = f'{text}\nВозникли ошибки при отправке {fail} пользователям.\nОшибки: {errors}'
+
+    await message.reply(text)
